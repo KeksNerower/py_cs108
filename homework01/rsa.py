594d17c85c310ebb08106cc9feef7926706e2b81
Nerower <p0sirega121234@gmail.com>
2020-12-31 19:39:37 +0300

    Homework: rsa: fixed

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -1,3 +1,4 @@
+import math
 import random
 import typing as tp
 
@@ -75,9 +76,11 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
 
     # n = pq
     # PUT YOUR CODE HERE
+    n = p * q
 
     # phi = (p-1)(q-1)
     # PUT YOUR CODE HERE
+    phi = (p-1) * (q-1)
 
     # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
