2b086cb654d44a2f832dd06727c8a951f77191a8
Nerower <p0sirega121234@gmail.com>
2021-04-17 15:38:36 +0300

    homework05: config: new access_token

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -1,5 +1,5 @@
 VK_CONFIG = {
     "domain": "https://api.vk.com/method",
-    "access_token": "1a2bb1d955c641b8790af8e73f9aa59d6d4dadd5664651f4a9c9591172aa69fd0b650c114dc3f1fda8845",
+    "access_token": "8da78d9163b2e7e43087df0f6ee79a509cf2111cbdd79e8e4d885a28311776ab1a749a59de23f9bead7df",
     "version": "5.124",
 }
