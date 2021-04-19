f1faf08b6c1f8fe7b51df7225408782f29b4ef7d
Nerower <p0sirega121234@gmail.com>
2021-04-19 03:41:39 +0300

    homework05: tests: plot communities

--- homework05/test.py
+++ homework05/test.py
@@ -1,54 +1,24 @@
-from vkapi.config import VK_CONFIG
-import requests
 import vkapi.friends as fr
-import matplotlib.pyplot as plt
 
+import community as community_louvain
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
 import networkx as nx
 
-# domain = VK_CONFIG["domain"]
-# access_token = VK_CONFIG["access_token"]
-# v = VK_CONFIG["version"]
-# # user_id = '459560428'
-# # fields = 'sex'
-# source_uid = '459560428'
-# target_uids = ['817934', '437397158']#['4744933']
-# order = ""
-# count = ""
-# offset = 0
 
-# # query = f"{domain}/friends.get?access_token={access_token}&user_id={user_id}&fields={fields}&v={v}"
-# query = f"{domain}/friends.getMutual?\
-#             access_token={access_token}&\
-#             source_uid={source_uid}&\
-#             target_uids={'' if (target_uids == None or target_uids == []) else ','.join(target_uids)}&\
-#             order={order}&\
-#             count={count}&\
-#             offset={offset}&\
-#             v={v}".replace(" ", "")
-# print(query)
-# response = requests.get(query)
-# print(response.json())
-
-
-
-# friends_response = fr.get_friends(user_id=817934, fields=["nickname"])
-# active_users = [user["id"] for user in friends_response.items if not user.get("deactivated") and not user.get("is_closed")]
-# print(len(active_users))
-
-# mutual_friends = fr.get_mutual(source_uid=817934, target_uids=active_users)
-
-# print(mutual_friends)
-
-options = {
-    'node_color': 'black',
-    'edge_color': 'grey',
-    'node_size': 25,
-    'width': 1,
-}
-
-net = fr.ego_network(user_id=817934)
+# Creating graph
+net = fr.ego_network(user_id=459560428)
 g = nx.Graph()
 g.add_edges_from(net)
-print(g)
-nx.draw(g, **options)
+
+# Finding partitios
+partition = community_louvain.best_partition(g)
+
+pos = nx.spring_layout(g)
+cmap = cm.get_cmap('viridis', max(partition.values()) + 1)
+nx.draw_networkx_nodes(g, pos, partition.keys(), node_size=20,
+                       cmap=cmap, node_color=list(partition.values()))
+nx.draw_networkx_edges(g, pos, alpha=0.5)
+
+# Save graph to picture
 plt.savefig("mygraph.png")
