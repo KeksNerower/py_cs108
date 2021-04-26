98a5c9e2d329baabf86a0fc18dfe3968b636aa4d
Nerower <p0sirega121234@gmail.com>
2021-04-26 22:46:21 +0300

    homework05: config: black fixes

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -1,5 +1,8 @@
+# https://hackernoon.com/4-ways-to-manage-the-configuration-in-python-4623049e841b
+
+# 7825774
 VK_CONFIG = {
     "domain": "https://api.vk.com/method",
-    "access_token": "b972cd3ea926ddb6439f4e73a151f1e4dd32ad54dd92dd16e7b357112614c7b0364f01bc920a096d378c8",
+    "access_token": "37b25562d5b59512c89ccea8abefca80f01221439553ebdb02d46e9e2828f8a07334462a70052fab8df19",
     "version": "5.124",
 }
